51ef3aeca235bdd82dc65c49f0cc85a7f09b6617
Esteban Aleman <esteban21112002@gmail.com>
2024-09-18 16:57:56 -0600

    Fixed nav

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -24,17 +24,16 @@ const navLinks = [
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
-  // Add this useEffect to dynamically load the font
   useEffect(() => {
     const link = document.createElement("link");
     link.href =
-      "https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap"; // Replace with your desired font
+      "https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap";
     link.rel = "stylesheet";
     document.head.appendChild(link);
   }, []);
 
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+    <nav className="fixed mx-auto border border-[#000000] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
