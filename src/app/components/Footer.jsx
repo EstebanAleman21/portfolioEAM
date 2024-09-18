75b7e9ad868ecf152c2de4f6c4e6148f167e6a95
Esteban Aleman <esteban21112002@gmail.com>
2024-09-18 15:20:36 -0600

    Fixes

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,25 @@
+"use client";
 import React from "react";
+import { useEffect } from "react";
 
 const Footer = () => {
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.href =
+      "https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap";
+    link.rel = "stylesheet";
+    document.head.appendChild(link);
+  }, []);
+
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
-        <p className="text-slate-600">All rights reserved.</p>
+        <h1
+          className="text-2xl text-blue-50"
+          style={{ fontFamily: "'Manrope'" }}
+        >
+          Esteban Aleman M.
+        </h1>
       </div>
     </footer>
   );
